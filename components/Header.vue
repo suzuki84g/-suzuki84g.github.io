8fa7f62c9d17be61f1eea9ab9bda111adcf38567
suzuki84g <suzuki.84g@gmail.com>
2020-08-25 11:54:57 +0900

    Headerを簡易的に作成

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Header
+  <div class="header">
+    <h1>ポートフォリオ</h1>
+    <div>navlist</div>
   </div>
 </template>
 
@@ -9,4 +10,11 @@ export default {}
 </script>
 
 <style lang="scss">
+.header {
+  h1 {
+    font: {
+      weight: bold;
+    }
+  }
+}
 </style>
