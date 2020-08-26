a3c087eb26e5d010971db6cc77d9584d91005d27
suzuki84g <suzuki.84g@gmail.com>
2020-08-26 15:17:05 +0900

    年齢を自動計算

--- components/Profile.vue
+++ components/Profile.vue
@@ -2,11 +2,11 @@
   <div class="profile">
     <h2>Plofile</h2>
     <p>{{ nameKanzi }} / {{ nameAlphabet }}</p>
-    <span>{{ birthday }}</span>
-    <spn>年齢</spn>
+    <span>{{ myBirthday.year }}年{{ myBirthday.month }}月{{ myBirthday.day }}日</span>
+    <span>年齢 {{ age }}</span>
     <figure class="photo">
-      <img src="~assets/image/YoutubeCutout.png" alt="近影">
-      <figcaption>写真右 2020年2月 スプラトゥーン甲子園関東地区大会</figcaption>
+      <img :src="recentPhotograph.imgSrc" alt="近影">
+      <figcaption>{{ recentPhotograph.caption }}</figcaption>
     </figure>
   </div>
 </template>
@@ -17,11 +17,38 @@ export default {
     return {
       nameKanzi: '鈴木 優紀',
       nameAlphabet: 'SUZUKI Yuki',
-      birthday: 19881027,
+      myBirthday: {
+        year: 1988,
+        month: 10,
+        day: 27
+      },
+      age: null,
+      calcDate: {
+        birthday: {},
+        today: {}
+      },
       recentPhotograph: {
-        picture: 'hoge',
-        caption: 'fuga'
+        imgSrc: require('@/assets/image/YoutubeCutout.png'),
+        caption: '写真右 2020年2月 スプラトゥーン甲子園関東地区大会にて'
       }
+    }
+  },
+  mounted () {
+    this.calcAge()
+  },
+  methods: {
+    dateTypeCasting (date, push) {
+      push.y = date.getFullYear().toString().padStart(4, '0')
+      push.m = (date.getMonth() + 1).toString().padStart(2, '0')
+      push.d = date.getDate().toString().padStart(2, '0')
+    },
+    calcAge () {
+      const birthDate = new Date(this.myBirthday.year, this.myBirthday.month - 1, this.myBirthday.day) // 誕生日のDateインスタンス化
+      const today = new Date() // 今日の日付を取得
+      const inData = this.calcDate
+      this.dateTypeCasting(birthDate, inData.birthday) // 引数渡して文字列に分解する
+      this.dateTypeCasting(today, inData.today) // 引数渡して文字列に分解する
+      this.age = Math.floor((Number(inData.today.y + inData.today.m + inData.today.d) - Number(inData.birthday.y + inData.birthday.m + inData.birthday.d)) / 10000) // 誕生日を計算
     }
   }
 }
