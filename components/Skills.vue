01bd8928f62b08e18ad628ef0567a2b6f358bb50
suzuki84g <suzuki.84g@gmail.com>
2020-09-04 03:05:00 +0900

    Columnでv-forによるリスト表記に成功

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="skill row">
+  <div class="skill row justify-content-center">
     <h2 class="col-12">
       Skill
     </h2>
-    <div v-for="skill in skills" :key="skill" class="col-12">
-      <p>{{ skill.language }} / Level {{ skill.level }}</p>
+    <div class="col-sm-3">
+      <p>フロント技術</p>
+      <ul>
+        <li v-for="front in skillSet.front" :key="front">
+          {{ front }}
+        </li>
+      </ul>
+    </div>
+    <div class="col-sm-3">
+      <p>サーバサイド技術</p>
+      <ul>
+        <li v-for="back in skillSet.back" :key="back">
+          {{ back }}
+        </li>
+      </ul>
+    </div>
+    <div class="col-sm-3">
+      <p>その他、インフラや開発ツール</p>
+      <ul>
+        <li v-for="other in skillSet.other" :key="other">
+          {{ other }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -13,16 +34,11 @@
 export default {
   data () {
     return {
-      skills: [
-        {
-          language: 'html',
-          level: '3'
-        },
-        {
-          language: 'css/sass',
-          level: '4'
-        }
-      ]
+      skillSet: {
+        front: ['html', 'css/sass', 'javascript', 'Bootstrap', 'Vue.js', 'Nuxt.js'],
+        back: ['Linux', 'WindowsServer', 'Python', 'Node.js', 'MySQL'],
+        other: ['git', 'GitHub', 'docker', 'Vagrant', 'AWS', 'Heroku', 'オンプレミス環境の物理作業', 'PMOの真似事など']
+      }
     }
   }
 }
