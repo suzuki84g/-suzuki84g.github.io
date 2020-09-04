e9f00ff8e1258e8e92b33e2d6b80834b22b6b6c1
suzuki84g <suzuki.84g@gmail.com>
2020-09-04 19:24:13 +0900

    取り敢えず作成

--- components/Link.vue
+++ components/Link.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="link row">
+  <div class="link row justify-content-center">
     <h2 class="col-12">
       Link
     </h2>
-    <div class="col-12">
-      <p>qiita</p>
-      <p>github</p>
-      <p>mail</p>
-    </div>
+    <a href="https://github.com/suzuki84g/" class="col-3">
+      github
+    </a>
+    <a href="https://qiita.com/" class="col-3">
+      qiita
+    </a>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfHFyNjfbH8tWLs9-GVh9Ax3QKBpFs6OwdVaAXmdTn183q7ew/viewform?usp=sf_link" class="col-3">
+      連絡先<br>
+      （googleフォーム）
+    </a>
   </div>
 </template>
 
