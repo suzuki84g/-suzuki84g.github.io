4fb99a29eb42926fb4f0db7d06cbdbda57548752
suzuki84g <suzuki.84g@gmail.com>
2020-09-04 19:27:33 +0900

    リンクを作成

--- components/Works.vue
+++ components/Works.vue
@@ -7,8 +7,8 @@
       <div class="card-body">
         <h4>{{ work.title }}</h4>
         <p>{{ work.about }}</p>
-        <p>{{ work.url }}</p>
-        <a :href="work.code">github</a>
+        <p><a :href="work.url">{{ work.url }}</a></p>
+        <p><a :href="work.code">github</a></p>
       </div>
     </div>
   </div>
@@ -19,8 +19,18 @@ export default {
   data () {
     return {
       works: [
-        { title: 'ポートフォリオ', about: 'このポートフォリオです。', url: 'https://suzuki84g.github.io/', code: 'https://github.com/suzuki84g/suzuki84g.github.io' },
-        { title: 'イカステージ', about: 'splatoon2のステージ情報を表示します。', url: 'https://ika-stage.herokuapp.com/index.html', code: 'https://github.com/suzuki84g/ikaStageSort' }
+        {
+          title: 'ポートフォリオ',
+          about: 'このポートフォリオです。',
+          url: 'https://suzuki84g.github.io/',
+          code: 'https://github.com/suzuki84g/suzuki84g.github.io'
+        },
+        {
+          title: 'イカステージ',
+          about: 'splatoon2のステージ情報を表示します。',
+          url: 'https://ika-stage.herokuapp.com/index.html',
+          code: 'https://github.com/suzuki84g/ikaStageSort'
+        }
       ]
     }
   }
