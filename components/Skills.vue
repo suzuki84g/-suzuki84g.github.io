247a7e3265f04c19d6aaedfc99935b026c211b03
suzuki84g <suzuki.84g@gmail.com>
2020-09-04 18:38:44 +0900

    レイアウトを修正

--- components/Skills.vue
+++ components/Skills.vue
@@ -3,27 +3,31 @@
     <h2 class="col-12">
       Skill
     </h2>
-    <div class="col-sm-3">
-      <p>フロント技術</p>
-      <ul>
-        <li v-for="front in skillSet.front" :key="front">
-          {{ front }}
+    <div class="col-10">
+      <ul class="row">
+        <li class="col-sm-4">
+          フロント技術
+          <ul>
+            <li v-for="front in skillSet.front" :key="front">
+              {{ front }}
+            </li>
+          </ul>
         </li>
-      </ul>
-    </div>
-    <div class="col-sm-3">
-      <p>サーバサイド技術</p>
-      <ul>
-        <li v-for="back in skillSet.back" :key="back">
-          {{ back }}
+        <li class="col-sm-4">
+          サーバサイド技術
+          <ul>
+            <li v-for="back in skillSet.back" :key="back">
+              {{ back }}
+            </li>
+          </ul>
         </li>
-      </ul>
-    </div>
-    <div class="col-sm-3">
-      <p>その他、インフラや開発ツール</p>
-      <ul>
-        <li v-for="other in skillSet.other" :key="other">
-          {{ other }}
+        <li class="col-sm-4">
+          その他、インフラや開発ツール
+          <ul>
+            <li v-for="other in skillSet.other" :key="other">
+              {{ other }}
+            </li>
+          </ul>
         </li>
       </ul>
     </div>
